9ff59af5270c9cbef7b209473865b7498d8bc933
DinoEater <160562476+DinoEater-TH@users.noreply.github.com>
2026-09-12 22:05:49 +0700

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
+// ============================================================
 // config.js — Frontend Configuration
 // ============================================================
 
 var CONFIG = {
-  API_URL: 'https://kku-memory-api-2.pongka.workers.dev',
   API_URL: 'https://kku-mem-api.pongka.workers.dev',
   GOOGLE_CLIENT_ID: '928908034346-boo594qv6vjeak7k9bj9o15cm35e0u3g.apps.googleusercontent.com',
   ALLOWED_DOMAIN: '@kku.ac.th'
