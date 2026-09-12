f1797f34e4404865e701389dd0bb0189597156c6
DinoEater <160562476+DinoEater-TH@users.noreply.github.com>
2026-09-12 14:44:39 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// ============================================================
-// config.js — Frontend Configuration
-// ============================================================
-
-var CONFIG = {
-  API_URL: 'https://script.google.com/a/macros/kku.ac.th/s/AKfycbxE8gBjSrr9AyMPRkLTX6pbY0BV5ePmn8-ZqTlP16DrWhHguRBUwL6yEaJ7ULyU194/exec',
-  GOOGLE_CLIENT_ID: '928908034346-boo594qv6vjeak7k9bj9o15cm35e0u3g.apps.googleusercontent.com',
-  ALLOWED_DOMAIN: '@kku.ac.th'
-};
