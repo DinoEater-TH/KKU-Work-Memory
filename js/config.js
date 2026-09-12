1cb84011bb20a2689a705dfe508b99ca5d44601e
DinoEater <160562476+DinoEater-TH@users.noreply.github.com>
2026-09-12 20:30:45 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,9 @@
-
-// ============================================================
 // config.js — Frontend Configuration
 // ============================================================
 
 var CONFIG = {
-  API_URL: 'https://script.google.com/a/macros/kku.ac.th/s/AKfycbxE8gBjSrr9AyMPRkLTX6pbY0BV5ePmn8-ZqTlP16DrWhHguRBUwL6yEaJ7ULyU194/exec',
+  API_URL: 'https://kku-memory-api-2.pongka.workers.dev',
+  API_URL: 'https://kku-mem-api.pongka.workers.dev',
   GOOGLE_CLIENT_ID: '928908034346-boo594qv6vjeak7k9bj9o15cm35e0u3g.apps.googleusercontent.com',
   ALLOWED_DOMAIN: '@kku.ac.th'
 };
